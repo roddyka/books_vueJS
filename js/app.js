3e894efec99c9e78f12085590da31a25bbb89b89
roddyka <rodrigoka@live.com>
2017-02-03 12:02:45 -0200

    Projeto Listagem utilizando VUEJS

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var hello = new Vue({
+  el: "#hello",
+  data: {
+    msg: "Hello Vue!",
+    peoples: [
+      {name:"Maria"},
+      {name:"Pedro"},
+      {name:"João"},
+      {name:"Ana"},
+      {name:"Gabriel"}
+    ]
+
+  }
+});
